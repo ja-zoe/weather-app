de35cfba822c67d1efc2ed124c82aa87b1cf3094
ja-zoe <j.vilfort01@gmail.com>
2024-06-28 04:57:00 -0400

    added nodemon script

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,9 @@ import axios from 'axios'
 import express from 'express'
 import cors from 'cors'
 
+const PORT = 8080
 const app = express()
 
 app.use(cors())
+
+app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
